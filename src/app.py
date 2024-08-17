62ca263ae2f7a345e73dd3bfd02efc5c6c4afd80
PauloHBSF <paulohbsfilho@gmail.com>
2024-08-17 13:32:37 -0300

    first commit

--- src/app.py
+++ src/app.py
@@ -1,5 +1,19 @@
 from faker import Faker
 from fastapi import FastAPI
+import random
 
-app = FastAPI()
-faker = Faker()
+app = FastAPI(debug=True)
+fake = Faker(locale=['PT-BR'])
+
+@app.get('/')
+async def home():
+    return [{'message': 'Hello World!'}]
+
+@app.get('/gerar_cliente')
+async def gera_funcionario():
+    return [{
+        'nome_cliente': fake.name(),
+        'doc_cliente': fake.cpf(),
+        'idade_cliente': random.randrange(18, 64),
+        'endereco_cliente': fake.address(),
+    }]
